de166d6a4aef3cbfe5f9e9715224a04c348d249c
Monsterdiscord <99562167+Monsterdiscord@users.noreply.github.com>
2023-01-26 19:11:41 -0500

    Fixed tos issues

--- main.py
+++ main.py
@@ -44,23 +44,17 @@ async def ban(ctx, member : discord.Member):
         await ctx.send("bot does not have the kick members permission!")
 
 
-@Monster.event
-async def on_message_delete(message):
-     snipe_message_author[message.channel.id] = message.author
-     snipe_message_content[message.channel.id] = message.content
-     await sleep(60)
-     del snipe_message_author[message.channel.id]
-     del snipe_message_content[message.channel.id]
+@Monster.command()
+async def credits(ctx):
+    embed=discord.Embed(title="**======== *Ghostface Credits* ========**", description=f"""
+                  Developer: 
 
-@Monster.command(name = 'snipe')
-async def snipe(ctx):
-    channel = ctx.channel
-    try: #This piece of code is run if the bot finds anything in the dictionary
-        em = discord.Embed(name = f"Last deleted message in #{channel.name}", description = snipe_message_content[channel.id])
-        em.set_footer(text = f"This message was sent by {snipe_message_author[channel.id]}")
-        await ctx.send(embed = em)
-    except: #This piece of code is run if the bot doesn't find anything in the dictionary
-        await ctx.send(f"There are no recently deleted messages in #{channel.name}")
+                  Sub-Developer:
+                  
+
+                  """, color=0xf20202)
+    embed.set_footer(text="Template made by Monstie#6632")
+    await ctx.send(embed=embed)
 
 
 
